5003964e92094c6ab15179636d619cec6410caee
Marianjuavi <mariana.ju92@gmail.com>
2023-05-25 02:36:42 -0300

    Se agrega el componente ItemTarea

---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,12 @@
+import {Button, ListGroup } from "react-bootstrap";
+
+const ItemTarea = () => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+      Cras justo odio
+      <Button variant='danger'>X</Button>
+    </ListGroup.Item>
+  );
+};
+
+export default ItemTarea;
