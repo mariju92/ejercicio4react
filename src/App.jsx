2f61b97cd61dc6cfa9a98c02aed51e1f8d66c5bc
Marianjuavi <mariana.ju92@gmail.com>
2023-05-25 00:46:29 -0300

    Se deja listo el maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -6,7 +7,15 @@ function App() {
 
   return (
     <>
-    
+    <h1 className='display-4 py-3 text-center bg-black text-light'>Lista de tareas</h1>
+    <Container className='mainPage'>
+      <hr className='text-light'/>
+      {/*aqui se agrega el formulario*/}
+    </Container>
+<footer className='bg-black text-secondary text-center py-2'>
+  <p>&copy; Todos los derechos reservados</p>
+</footer>
+
     </>
   )
 }
