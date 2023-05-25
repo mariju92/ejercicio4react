d5d9b4cfa1eb3928ff6c7258158c9dd1c137e77c
Marianjuavi <mariana.ju92@gmail.com>
2023-05-25 05:18:47 -0300

    Se agrega tarea al array Tareas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,32 @@
-import Form from 'react-bootstrap/Form'
-import ListaTareas from './ListaTareas';
+import Form from "react-bootstrap/Form";
+import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 function Formulario() {
-    return (
-        <section>
-            <Form>
-                <Form.Group className="mb-3" controlId="Tarea">
-                    <Form.Control type="text" placeholder="Ingresar Tarea" />
-                </Form.Group>
-            </Form>
-            <ListaTareas/>
-        </section>
-    );
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...tareas, tarea]);
+    setTarea('');
+  };
+
+  return (
+    <section>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="Tarea">
+          <Form.Control
+            type="text"
+            placeholder="Ingresar Tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+        </Form.Group>
+      </Form>
+      <ListaTareas />
+    </section>
+  );
 }
 
 export default Formulario;
