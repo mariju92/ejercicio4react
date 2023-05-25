ac68303cbb98915380ab7b920f75b7f5831e59d8
Marianjuavi <mariana.ju92@gmail.com>
2023-05-25 02:16:32 -0300

    Se agrega el componente listaTareas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,16 @@
 import Form from 'react-bootstrap/Form'
+import ListaTareas from './ListaTareas';
 
 function Formulario() {
     return (
-        <div>
+        <section>
             <Form>
                 <Form.Group className="mb-3" controlId="Tarea">
                     <Form.Control type="text" placeholder="Ingresar Tarea" />
                 </Form.Group>
             </Form>
-        </div>
+            <ListaTareas/>
+        </section>
     );
 }
 
